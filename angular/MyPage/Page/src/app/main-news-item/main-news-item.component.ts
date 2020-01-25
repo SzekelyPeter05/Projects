207be13ade9f3993bf91730d426595344abc4ca8
SzekelyPeter05 <szekelypeter05@gmail.com>
2020-01-25 17:08:06 +0100

    commit

--- angular/MyPage/Page/src/app/main-news-item/main-news-item.component.ts
+++ angular/MyPage/Page/src/app/main-news-item/main-news-item.component.ts
@@ -12,7 +12,7 @@ export class MainNewsItemComponent implements OnInit {
 
   newsList:newsType[] = this.DataSourceNews.getNews();
   constructor(private DataSourceNews: DataSourceNews,public breakpointObserver: BreakpointObserver) { }
-  
+  size:boolean[] =  [true,false,false,false,false];
 
   ngOnInit() {
     this.setImageSize();
@@ -28,26 +28,31 @@ export class MainNewsItemComponent implements OnInit {
       if (state.breakpoints[Breakpoints.XSmall]) {
         for(let index in this.newsList)
           { 
-              this.newsList[index]['picSrc'] = this.newsList[index]['pictureSrcSmall'];  
+              this.newsList[index]['picSrc'] = this.newsList[index]['pictureSrcSmall']; 
+             
           }
+          this.size =  [false,false,false,false,true];
       }
       if (state.breakpoints[Breakpoints.Small]) {
         for(let index in this.newsList)
           { 
               this.newsList[index]['picSrc'] = this.newsList[index]['pictureSrcSmall'];  
           }
+          this.size =  [false,false,false,true,false];
       }
       if (state.breakpoints[Breakpoints.Medium]) {
         for(let index in this.newsList)
         { 
             this.newsList[index]['picSrc'] = this.newsList[index]['pictureSrcMedium'];  
         }
+        this.size =  [false,false,true,false,false];
       }
       if (state.breakpoints[Breakpoints.Large]) {
         for(let index in this.newsList)
         { 
             this.newsList[index]['picSrc'] = this.newsList[index]['pictureSrc'];  
         }
+        this.size =  [false,true,false,false,false];
       
       }
       if (state.breakpoints[Breakpoints.XLarge]) {
@@ -55,6 +60,7 @@ export class MainNewsItemComponent implements OnInit {
         { 
             this.newsList[index]['picSrc'] = this.newsList[index]['pictureSrc'];  
         }
+        this.size =  [true,false,false,false,false];
         
       }
     });
